117c74713ce61080d0f83c40fa49c70283f306f7
Santiago Ramirez <santi_coding@outlook.com>
2021-08-09 20:23:10 -0400

    Empezando prueba pre-entrevista NOTUS

---
+++ src/components/TheNavBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    id="nav"
+    class="sticky flex items-center p-3 space-x-3 bg-blue-500 w-full h-14"
+  >
+    <router-link to="/">Home</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class="text-3xl text-indigo-500">{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Vista Principal',
+    };
+  },
+};
+</script>
